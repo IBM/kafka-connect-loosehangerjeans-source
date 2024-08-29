f975248deb227691174c023b72ec883edac62f8f
Dale Lane <dale.lane@uk.ibm.com>
2024-08-29 20:27:48 +0100

    chore: add missing copyright header
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- src/main/java/com/ibm/eventautomation/demos/loosehangerjeans/data/OrdersAndCancellations.java
+++ src/main/java/com/ibm/eventautomation/demos/loosehangerjeans/data/OrdersAndCancellations.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2024 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.ibm.eventautomation.demos.loosehangerjeans.data;
 
 import java.util.List;
@@ -6,7 +21,7 @@ public class OrdersAndCancellations {
 
     private final List<Order> orderList;
     private final List<Cancellation> cancellationList;
-    
+
     public OrdersAndCancellations(List<Order> orderList, List<Cancellation> cancellationList) {
         this.orderList = orderList;
         this.cancellationList = cancellationList;
@@ -20,6 +35,6 @@ public class OrdersAndCancellations {
         return cancellationList;
     }
 
-    
-    
+
+
 }
