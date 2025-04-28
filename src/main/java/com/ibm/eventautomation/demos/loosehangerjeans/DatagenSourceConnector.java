b3f812fa96845c4aa987019b48a95b55740c545b
Dale Lane <dale.lane@uk.ibm.com>
2025-04-28 22:41:37 +0100

    chore: update dependencies
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- src/main/java/com/ibm/eventautomation/demos/loosehangerjeans/DatagenSourceConnector.java
+++ src/main/java/com/ibm/eventautomation/demos/loosehangerjeans/DatagenSourceConnector.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
 
 public class DatagenSourceConnector extends SourceConnector {
 
-    protected static final String VERSION = "0.3.0";
+    protected static final String VERSION = "0.3.1";
 
     private final Logger log = LoggerFactory.getLogger(DatagenSourceConnector.class);
 
